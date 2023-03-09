62f1d8b0555f0293542916e0e7b1f652de55c099
Jared Bienz <jbienz@hotmail.com>
2023-03-09 15:15:34 -0600

    Nav Demo fully functional for single user. Needs testing.

--- XRStarterKit/Assets/XRStarterDemos/Navigation/Scripts/NavVisualizer.cs
+++ XRStarterKit/Assets/XRStarterDemos/Navigation/Scripts/NavVisualizer.cs
@@ -15,7 +15,46 @@ public class NavVisualizer : MonoBehaviour
     [Tooltip("The agent to visualize navigation for.")]
     [SerializeField]
     private NavMeshAgent _agent;
+
+    [Tooltip("The line renderer used to visualize navigation segments.")]
+    [SerializeField]
+    private LineRenderer _lineRenderer;
     #endregion // Unity Inspector Variables
+
+    /// <summary>
+    /// Draws the current path of the nav agent.
+    /// </summary>
+    private void DrawPath()
+    {
+        // If the agent doesn't have a path, or there's only a sigle point, draw nothing
+        if ((!_agent.hasPath) || (_agent.path.corners.Length < 2))
+        {
+            // No points
+            _lineRenderer.positionCount = 0;
+
+            // Not enabled
+            _lineRenderer.enabled = false;
+
+            // Done
+            return;
+        }
+
+        // Is enabled
+        _lineRenderer.enabled = true;
+
+        // Set the number of positions
+        _lineRenderer.positionCount = _agent.path.corners.Length;
+
+        // The first position is the next point of the agent
+        _lineRenderer.SetPosition(0, _agent.nextPosition);
+
+        // Draw remaining points
+        for (int iPoint = 1; iPoint < _agent.path.corners.Length; iPoint++) 
+        {
+            _lineRenderer.SetPosition(iPoint, _agent.path.corners[iPoint]);
+        }
+    }
+
 
     #region Unity Message Handlers
     /// <summary>
@@ -23,6 +62,10 @@ public class NavVisualizer : MonoBehaviour
     /// </summary>
     private void Start()
     {
+        /*****************
+         * Agent
+         *****************/
+
         // We let the camera move freely and not controlled by the agent.
         _agent.updatePosition = false;
         _agent.updateRotation = false;
@@ -37,6 +80,18 @@ public class NavVisualizer : MonoBehaviour
 
         // We do want the agent to constantly repath
         _agent.autoRepath = true;
+
+
+        /*****************
+         * Line Renderer
+         *****************/
+
+        // We want the line renderer to start with no segments
+        _lineRenderer.positionCount = 0;
+
+        // Want a fairly narrow line
+        _lineRenderer.startWidth = 0.1f;
+        _lineRenderer.endWidth = 0.1f;
     }
 
     /// <summary>
@@ -46,6 +101,9 @@ public class NavVisualizer : MonoBehaviour
     {
         // Always update the agent position to match the current object position
         _agent.nextPosition = transform.position;
+
+        // Draw the path
+        DrawPath();
     }
     #endregion // Unity Message Handlers
 }
